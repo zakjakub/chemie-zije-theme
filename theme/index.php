895f28d5e43dc069b562519126ae45b764bcdbc8
zakjakub <mail@jakubzak.eu>
2021-01-03 20:54:23 +0100

    Fixed some things.

--- theme/index.php
+++ theme/index.php
@@ -7,12 +7,11 @@
  * It is used to display a page when nothing more specific matches a query.
  * E.g., it puts together the home page when no home.php file exists
  */
-$context          = Timber::context();
-$context['posts'] = new Timber\PostQuery(new WP_Query());
-$context['foo']   = 'bar';
-$templates        = array('post-types/index.html.twig');
-if (is_home()) {
+$context   = Timber::context();
+$templates = ['post-types/index.html.twig'];
+if (is_home() || is_front_page()) {
     $context['isHome']   = true;
-    array_unshift($templates, 'post-types/front-page.html.twig', 'post-types/home.html.twig');
+    $context['articles'] = Timber::get_posts(['post_type' => 'article',]);
+    $templates           = ['post-types/home.html.twig', 'post-types/home.html.twig', ...$templates];
 }
 Timber::render($templates, $context);
