fdedf01779255f3d5fb5b66e23fb71ac3391e3b3
zakjakub <mail@jakubzak.eu>
2021-01-11 21:30:09 +0100

    Fixed some things.

--- theme/page-kontakt.php
+++ theme/page-kontakt.php
@@ -3,5 +3,6 @@
 $context             = Timber::context();
 $context['post']     = Timber::get_post();
 $context['contacts'] = Timber::get_posts(['post_type' => 'contact_person',]);
+$context['contact'] = carbon_get_theme_option('contact');
 $templates           = ['page-customs/page-'.$context['post']->slug.'.html.twig', 'post-types/page.html.twig'];
 Timber::render($templates, $context);
