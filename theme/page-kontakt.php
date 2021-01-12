1fc8450a651f72438d8d9a3aaf88a9540cbe29f3
zakjakub <mail@jakubzak.eu>
2021-01-12 23:31:47 +0100

    Fixed some things.

--- theme/page-kontakt.php
+++ theme/page-kontakt.php
@@ -1,8 +1,24 @@
 <?php
 
-$context             = Timber::context();
-$context['post']     = Timber::get_post();
-$context['contacts'] = Timber::get_posts(['post_type' => 'contact_person',]);
-$context['contact'] = carbon_get_theme_option('contact');
-$templates           = ['page-customs/page-'.$context['post']->slug.'.html.twig', 'post-types/page.html.twig'];
+use Timber\Post;
+use Timber\Term;
+
+$context                = Timber::context();
+$context['post']        = Timber::get_post();
+$context['contacts']    = carbon_get_theme_option('contact');
+$context['departments'] = [];
+foreach (Timber::get_posts(['post_type' => 'contact_person']) as $person) {
+    assert($person instanceof Post);
+    $departments = $person->terms('sub_department');
+    if (empty($departments)) {
+        $departments[] = 'Další kontakty';
+    }
+    foreach ($departments as $department) {
+        assert($department instanceof Term);
+        $context['departments'][$department->path()]['name']      = $department->title();
+        $context['departments'][$department->path()]['persons'][] = $person;
+    }
+}
+$context['departments'] = array_reverse($context['departments']);
+$templates = ['page-customs/page-'.$context['post']->slug.'.html.twig', 'post-types/page.html.twig'];
 Timber::render($templates, $context);
