3cd1735ba65486e368a221af6e20c805a95f7580
Jakub Žák <mail@jakubzak.eu>
2021-04-05 23:29:46 +0200

    Added EU MSMT widget.

--- theme/page.php
+++ theme/page.php
@@ -12,6 +12,10 @@
  * /mytheme/page-mypage.php (in which case you'll want to duplicate this file and save to the above path)
  */
 $context = Timber::context();
+$widgets = [''];
+foreach ($widgets as $widget) {
+    $context['sidebars'][$widget] = Timber::get_widgets($widget);
+}
 $postSlug = $context['post']->slug;
 $templates = ["page-customs/page-$postSlug.html.twig", 'post-types/page.html.twig'];
 if (is_home() || is_front_page()) {
