2340b908f9393d8c1e52a2e77d71331b3192962c
Jakub Žák <mail@jakubzak.eu>
2021-05-26 00:17:42 +0200

    Some changes.

--- src/Widgets/DepartmentContactsWidget.php
+++ src/Widgets/DepartmentContactsWidget.php
@@ -18,6 +18,21 @@ class DepartmentContactsWidget extends WP_Widget
 
     final public function widget($args, $instance): void
     {
-        Timber::render('widgets/department-contacts-widget/department-contacts-widget.html.twig', Timber::context());
+        $context = Timber::context();
+        $context['contact'] = [
+            'name' => carbon_get_theme_option('contact_name'),
+            'department' => carbon_get_theme_option('contact_department'),
+            'faculty' => carbon_get_theme_option('contact_faculty'),
+            'university' => carbon_get_theme_option('contact_university'),
+            'street' => carbon_get_theme_option('contact_street'),
+            'house_number' => carbon_get_theme_option('contact_house_number'),
+            'postal_code' => carbon_get_theme_option('contact_postal_code'),
+            'city' => carbon_get_theme_option('contact_city'),
+            'phone' => carbon_get_theme_option('contact_phone'),
+            'fax' => carbon_get_theme_option('contact_fax'),
+            'e_mail' => carbon_get_theme_option('contact_e_mail'),
+            'gps' => carbon_get_theme_option('contact_gps'),
+        ];
+        Timber::render('widgets/department-contacts-widget/department-contacts-widget.html.twig', $context);
     }
 }
