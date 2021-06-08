0a8833ec5ae1ed54cf0fce2c08b2abad8ce6b62c
Jakub Žák <mail@jakubzak.eu>
2021-06-08 15:20:20 +0200

    Added calculation category page.

---
+++ theme/single-chemical_calculation_category.php
@@ -0,0 +1,5 @@
+<?php
+
+$context = Timber::context();
+$templates = ['single-customs/single-chemical-calculation-category.html.twig', 'post-types/page.html.twig'];
+Timber::render($templates, $context);
