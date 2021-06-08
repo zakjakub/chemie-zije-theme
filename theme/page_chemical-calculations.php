efdc83e44a121f3778f76f5417d0328989e4f3a7
Jakub Žák <mail@jakubzak.eu>
2021-06-08 17:11:20 +0200

    Added calculation category page.

--- theme/page_chemical-calculations.php
+++ theme/page_chemical-calculations.php
@@ -1,10 +1,10 @@
 <?php
 /*
  * Template Name: Stránka s kategoriemi chemických výpočtů
- * Template Post Type: page
+ * Template Post Type: study_material_cat
  */
 
 $context = Timber::context();
-$context['posts'] = Timber::get_posts(['post_type' => 'calculation_category']);
+$context['posts'] = Timber::get_posts(['post_type' => 'equation_category']);
 $templates = ['custom-templates/chemical-calculations.html.twig', 'post-types/page.html.twig'];
 Timber::render($templates, $context);
