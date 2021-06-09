07da36184fee305d3f68b93936dbd1c56f7811e9
Jakub Žák <mail@jakubzak.eu>
2021-06-09 07:18:18 +0200

    Some changes.

---
+++ theme/dist/app/main-41d96233.js
@@ -0,0 +1,19 @@
+/*!
+ * 
+ * chemieZijeTheme
+ * 
+ * @author Jakub ZAK
+ * @version 0.1.0
+ * @link UNLICENSED
+ * @license UNLICENSED
+ * 
+ * Copyright (c) 2021 Jakub ZAK
+ * 
+ * This software is released under the UNLICENSED License
+ * https://opensource.org/licenses/UNLICENSED
+ * 
+ * Compiled with the help of https://wpack.io
+ * A zero setup Webpack Bundler Script for WordPress
+ */
+(window.wpackiochemieZijeThemeappJsonp=window.wpackiochemieZijeThemeappJsonp||[]).push([[0],[,,function(i,n,e){e(3),i.exports=e(4)},,function(i,n,e){"use strict";e.r(n);e(6);var o=e(1);e(5),new o.a(".swiper-container",{direction:"horizontal",loop:!1,pagination:{el:".swiper-pagination"},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}})},function(i,n,e){}],[[2,1,2]]]);
+//# sourceMappingURL=main-41d96233.js.map
