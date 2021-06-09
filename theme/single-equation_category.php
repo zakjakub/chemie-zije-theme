b861cfe839540e73786eed597631bbd33c66b86b
Jakub Žák <mail@jakubzak.eu>
2021-06-09 06:56:55 +0200

    Added filters processing for content of equations.

--- theme/single-equation_category.php
+++ theme/single-equation_category.php
@@ -2,4 +2,5 @@
 
 $context = Timber::context();
 $templates = ['single-customs/single-equation-category.html.twig', 'post-types/page.html.twig'];
+$context['equations'] = carbon_get_the_post_meta('solved_equations');
 Timber::render($templates, $context);
