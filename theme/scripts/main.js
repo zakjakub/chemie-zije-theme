4ffc7b44f5f42ac50e813bb61993ab4ed3507776
Jakub Žák <mail@jakubzak.eu>
2021-06-09 07:16:46 +0200

    Added filters processing for content of equations.

--- theme/scripts/main.js
+++ theme/scripts/main.js
@@ -1,4 +1,18 @@
-// noinspection ES6UnusedImports
+// noinspection ES6UnusedImports,JSUnusedLocalSymbols
 // noinspection ES6CheckImport
 import bootstrap from 'bootstrap';
+import Swiper from 'swiper/bundle';
 import '../styles/main.scss';
+
+const swiper = new Swiper('.swiper-container', {
+    direction: 'horizontal',
+    loop: false,
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
