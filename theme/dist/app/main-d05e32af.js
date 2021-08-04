a96a5ccd86826ec44c73b80de56340e871c9e7bc
Jakub Zak <mail@jakubzak.eu>
2021-08-04 22:26:13 +0200

    Updated dependencies.

---
+++ theme/dist/app/main-d05e32af.js
@@ -0,0 +1,19 @@
+/*!
+ * 
+ * chemieZijeTheme
+ * 
+ * @author Jakub ZAK
+ * @version 0.1.0
+ * @link UNLICENSED
+ * @license UNLICENSED
+ * 
+ * Copyright (c) 2021 Jakub ZAK
+ * 
+ * This software is released under the UNLICENSED License
+ * https://opensource.org/licenses/UNLICENSED
+ * 
+ * Compiled with the help of https://wpack.io
+ * A zero setup Webpack Bundler Script for WordPress
+ */
+(window.wpackiochemieZijeThemeappJsonp=window.wpackiochemieZijeThemeappJsonp||[]).push([[0],{3:function(i,e,n){n(4),i.exports=n(6)},6:function(i,e,n){"use strict";n.r(e);n(5),new(n(2).a)(".swiper-container",{direction:"horizontal",loop:!1,pagination:{el:".swiper-pagination"},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}})}},[[3,1,2]]]);
+//# sourceMappingURL=main-d05e32af.js.map
