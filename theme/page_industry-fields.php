9f521a020ee4d7802b4ed4219a036d39015ea683
Jakub Zak <mail@jakubzak.eu>
2021-08-04 22:22:49 +0200

    Updated dependencies.

--- theme/page_industry-fields.php
+++ theme/page_industry-fields.php
@@ -9,6 +9,7 @@ $context['posts'] = Timber::get_posts(
     [
         'post_type' => 'industry_field',
         'orderby'   => ['priority' => 'ASC'],
+        'posts_per_page' => 100,
     ]
 );
 $templates = ['custom-templates/industry-fields.html.twig', 'post-types/page.html.twig'];
