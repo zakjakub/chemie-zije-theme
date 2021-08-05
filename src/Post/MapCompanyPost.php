50fac4567ade73ec57e2e1e401f4237a51d4fe7b
Jakub Zak <mail@jakubzak.eu>
2021-08-06 00:06:02 +0200

    Added map.

--- src/Post/MapCompanyPost.php
+++ src/Post/MapCompanyPost.php
@@ -6,8 +6,18 @@ use Timber\Post;
 
 class MapCompanyPost extends Post
 {
+    final public function documents(): array
+    {
+        return carbon_get_the_post_meta('documents') ?? [];
+    }
+
     final public function locations(): array
     {
         return carbon_get_the_post_meta('locations') ?? [];
+    }
+
+    final public function activities(): array
+    {
+        return carbon_get_the_post_meta('activities') ?? [];
     }
 }
---
+++ theme/single-map_company.php
@@ -0,0 +1,3 @@
+<?php
+
+Timber::render(['single-customs/single-map-company.html.twig', 'post-types/page.html.twig'], Timber::context());
