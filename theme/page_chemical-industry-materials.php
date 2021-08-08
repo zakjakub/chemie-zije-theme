41ed2130a201494eb84642d3a38640533db36275
Jakub Zak <mail@jakubzak.eu>
2021-08-08 21:11:12 +0200

    Added map.

--- theme/page_chemical-industry-materials.php
+++ theme/page_chemical-industry-materials.php
@@ -7,6 +7,12 @@
 
 $context = Timber::context();
 $postSlug = $context['post']->slug;
-$context['elements'] = Timber::get_posts(['post_type' => 'industry_material']);
+$context['elements'] = Timber::get_posts(
+    [
+        'post_type' => 'industry_material',
+        'orderby'   => ['priority' => 'ASC'],
+        'posts_per_page' => 100,
+    ]
+);
 $templates = ["custom-templates/industry-materials.html.twig", 'post-types/page-layout.html.twig'];
 Timber::render($templates, $context);
