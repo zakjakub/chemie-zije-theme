774997d43e6e10a0aea64b9016cd19486386156d
Jakub Zak <mail@jakubzak.eu>
2021-10-03 19:50:06 +0200

    Did some changes based on updated specification.

--- theme/single-teach_material.php
+++ theme/single-teach_material.php
@@ -1,5 +1,6 @@
 <?php
 
 $context = Timber::context();
+$context['subtype'] = get_query_var('oblast');
 $templates = ['post-types/teach_material.html.twig', 'post-types/page.html.twig'];
 Timber::render($templates, $context);
