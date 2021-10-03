f88d775d77d1ad448c126bd366a838d7ce14a2d1
Jakub Zak <mail@jakubzak.eu>
2021-10-03 22:35:39 +0200

    Did some changes based on updated specification.

--- theme/single-map_company.php
+++ theme/single-map_company.php
@@ -1,7 +1,4 @@
 <?php
 $context = Timber::context();
 $templates = ['post-types/single-map_company.html.twig', 'post-types/page.html.twig'];
-
-dd($context);
-
 Timber::render($templates, $context);
--- theme/single-teach_material.php
+++ theme/single-teach_material.php
@@ -2,7 +2,4 @@
 
 $context = Timber::context();
 $templates = ['post-types/teach_material.html.twig', 'post-types/page.html.twig'];
-
-dd($context);
-
 Timber::render($templates, $context);
