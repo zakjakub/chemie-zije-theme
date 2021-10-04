28703d706ad72bb884a28220d60ed524cebc63fb
Jakub Zak <mail@jakubzak.eu>
2021-10-04 13:18:06 +0200

    Did some changes based on updated specification.

--- theme/page_teaching-material-category-photos.php
+++ theme/page_teaching-material-category-photos.php
@@ -31,11 +31,6 @@ $context['materials'] = Timber::get_posts(
                 'field'    => 'title',
                 'terms'    => array_map(static fn(Term $term) => $term->__toString(), $context['categories']),
             ],
-            [
-                'taxonomy' => 'teach_mat_sub_type',
-                'field'    => 'slug',
-                'terms'    => $subTerm ? ''.$context['subtype'] : [],
-            ],
         ],
     ])
 );
