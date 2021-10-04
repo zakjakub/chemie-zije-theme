4ee7a90b86051eef83c49c451f2d41cd2a364feb
Jakub Zak <mail@jakubzak.eu>
2021-10-04 13:29:39 +0200

    Did some changes based on updated specification.

--- theme/page_chemical-equations.php
+++ theme/page_chemical-equations.php
@@ -5,6 +5,6 @@
  */
 
 $context = Timber::context();
-$context['posts'] = Timber::get_posts(['post_type' => 'equation_category']);
+$context['posts'] = Timber::get_posts(['post_type' => 'equation_category', 'numberposts' => -1]);
 $templates = ['custom-templates/chemical-calculations.html.twig', 'post-types/page.html.twig'];
 Timber::render($templates, $context);
