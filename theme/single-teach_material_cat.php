130b3dec8b06146ebb0f0ccdbcf4ddf90ef42da4
Jakub Zak <mail@jakubzak.eu>
2021-10-31 23:26:08 +0100

    Some changes based on new requirements.

--- theme/single-teach_material_cat.php
+++ theme/single-teach_material_cat.php
@@ -3,20 +3,27 @@
 use Timber\Term;
 
 $context = Timber::context();
-$context['subtype'] = get_query_var('oblast');
+$context['subtype'] = get_query_var('oblast', null);
 $templates = ['post-types/teach_material_cat.html.twig', 'post-types/page.html.twig'];
 $terms = $context['post']->terms('teach_mat_cat_type');
 $context['subtypes'] = $context['post']->terms('teach_mat_sub_type');
+usort($context['subtypes'], static fn(Term $a, Term $b) => $a->description() <=> $b->description());
 $context['materials'] = Timber::get_posts(
     new WP_Query([
         'post_type' => 'teach_material',
         'orderby'   => 'title',
         'order'     => 'ASC',
         'tax_query' => [
+            'relation' => 'AND',
             [
                 'taxonomy' => 'teach_mat_cat_type',
                 'field'    => 'slug',
                 'terms'    => array_map(static fn(Term $term) => $term->__toString(), $terms),
+            ],
+            [
+                'taxonomy' => 'teach_mat_sub_type',
+                'field'    => 'slug',
+                'terms'    => [$context['subtype']],
             ],
         ],
     ])
