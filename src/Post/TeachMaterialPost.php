053f9a3da27c3a3436f392716970c8ecb6fe356a
Jakub Zak <mail@jakubzak.eu>
2021-11-09 17:07:12 +0100

    Some changes based on new requirements.

--- src/Post/TeachMaterialPost.php
+++ src/Post/TeachMaterialPost.php
@@ -8,6 +8,19 @@ class TeachMaterialPost extends Post
 {
     final public function documents(): array
     {
-        return carbon_get_the_post_meta('documents') ?? [];
+        return array_merge(
+            $this->presentations(),
+            $this->handouts(),
+        );
+    }
+
+    final public function presentations(): array
+    {
+        return carbon_get_the_post_meta('presentation') ?? [];
+    }
+
+    final public function handouts(): array
+    {
+        return carbon_get_the_post_meta('handouts') ?? [];
     }
 }
