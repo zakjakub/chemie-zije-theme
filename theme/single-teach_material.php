8b34bba12c0381c1ee84838aa4ffb3d8ef5b1227
Jakub Zak <mail@jakubzak.eu>
2021-11-09 16:38:21 +0100

    Some changes based on new requirements.

--- theme/single-teach_material.php
+++ theme/single-teach_material.php
@@ -1,5 +1,9 @@
 <?php
 
+use Timber\Term;
+
 $context = Timber::context();
+$context['categories'] = $context['post']->terms('teach_mat_cat_type');
+$context['subtitle'] = implode(', ', array_map(static fn(Term $term) => $term->title(), $context['categories']));
 $templates = ['post-types/teach_material.html.twig', 'post-types/page.html.twig'];
 Timber::render($templates, $context);
