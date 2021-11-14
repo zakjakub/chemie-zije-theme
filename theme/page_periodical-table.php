ade7e2c081d770e8a76f0017b3cb6c93c5c21df3
Jakub Zak <mail@jakubzak.eu>
2021-11-15 00:02:55 +0100

    Implemented new/updated requirements.

--- theme/page_periodical-table.php
+++ theme/page_periodical-table.php
@@ -24,9 +24,6 @@ if (GSHEET_URL) {
     }
 }
 
-die(var_export($cells['tbody']['tr'], true));
-
-
 foreach (isset($cells) && is_array($cells) ? $cells['tbody']['tr'] : [] as $rowIndex => $row) {
     $columnIndex = 'A';
 
