92a26a49df15b0cecfde35fb051bcf5453666b85
Jakub Zak <mail@jakubzak.eu>
2021-11-14 23:36:07 +0100

    Implemented new/updated requirements.

--- theme/includes/elements-gsheet.php
+++ theme/includes/elements-gsheet.php
@@ -1,39 +1,39 @@
 <?php
-
-/**
- * @param  null  $url
- *
- * @return array
- * @throws \Exception
- */
-function google_sheet($url = null): array
-{
-    $array = [];
-    if ($url) {
-        $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_HEADER, 0);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        $sheet = curl_exec($curl);
-        curl_close($curl);
-        preg_match('/(<table[^>]+>)(.+)(<\/table>)/', $sheet, $matches);
-        $data = $matches['0'];
-        $cells_xml = new SimpleXMLElement($data);
-        $cells_json = json_encode($cells_xml, JSON_THROW_ON_ERROR);
-        $cells = json_decode($cells_json, true, 512, JSON_THROW_ON_ERROR);
-    }
-    if (isset($cells) && is_array($cells)) {
-        foreach ($cells['tbody']['tr'] as $row => $row_data) {
-            $column = 'A';
-            foreach ($row_data['td'] as $column_index => $cell) {
-                if (!is_array($cell)) {
-                    $array[($row + 1)][$column++] = $cell;
-                } elseif ($cell['div']) {
-                    $array[($row + 1)][$column++] = $cell['div'];
-                }
-            }
-        }
-    }
-
-    return $array;
-}
+//
+///**
+// * @param  null  $url
+// *
+// * @return array
+// * @throws \Exception
+// */
+//function google_sheet($url = null): array
+//{
+//    $array = [];
+//    if ($url) {
+//        $curl = curl_init();
+//        curl_setopt($curl, CURLOPT_URL, $url);
+//        curl_setopt($curl, CURLOPT_HEADER, 0);
+//        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+//        $sheet = curl_exec($curl);
+//        curl_close($curl);
+//        preg_match('/(<table[^>]+>)(.+)(<\/table>)/', $sheet, $matches);
+//        $data = $matches['0'];
+//        $cells_xml = new SimpleXMLElement($data);
+//        $cells_json = json_encode($cells_xml, JSON_THROW_ON_ERROR);
+//        $cells = json_decode($cells_json, true, 512, JSON_THROW_ON_ERROR);
+//    }
+//    if (isset($cells) && is_array($cells)) {
+//        foreach ($cells['tbody']['tr'] as $row => $row_data) {
+//            $column = 'A';
+//            foreach ($row_data['td'] as $column_index => $cell) {
+//                if (!is_array($cell)) {
+//                    $array[($row + 1)][$column++] = $cell;
+//                } elseif ($cell['div']) {
+//                    $array[($row + 1)][$column++] = $cell['div'];
+//                }
+//            }
+//        }
+//    }
+//
+//    return $array;
+//}
--- theme/page_periodical-table.php
+++ theme/page_periodical-table.php
@@ -5,10 +5,40 @@
  * Template Post Type: page, study_material_cat, teach_material_cat
  */
 
-require_once "includes/elements-gsheet.php";
-require_once "includes/elements.php";
+const GSHEET_URL = 'https://docs.google.com/spreadsheets/d/1QUGqq7ZxccdSeSu5KVp07AVSUjbFVApMGrCIem0qRGA/edit';
+$elements = [];
+if (GSHEET_URL) {
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, GSHEET_URL);
+    curl_setopt($curl, CURLOPT_HEADER, 0);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    $sheet = curl_exec($curl);
+    curl_close($curl);
+    preg_match('/(<table[^>]+>)(.+)(<\/table>)/', $sheet, $matches);
+    $data = $matches['0'];
+    try {
+        $cells_xml = new SimpleXMLElement($data);
+        $cells_json = json_encode($cells_xml, JSON_THROW_ON_ERROR);
+        $cells = json_decode($cells_json, true, 512, JSON_THROW_ON_ERROR);
+    } catch (Exception $e) {
+    }
+}
+if (isset($cells) && is_array($cells)) {
+    foreach ($cells['tbody']['tr'] as $row => $row_data) {
+        $column = 'A';
+        foreach ($row_data['td'] as $column_index => $cell) {
+            if (!is_array($cell)) {
+                $array[($row + 1)][$column++] = $cell;
+            } elseif ($cell['div']) {
+                $array[($row + 1)][$column++] = $cell['div'];
+            }
+        }
+    }
+}
+
+die(var_export($elements, true));
 
 $context = Timber::context();
-$context['elements'] = $oldElements;
+$context['elements'] = $elements;
 $templates = ["custom-templates/periodical-table.html.twig", 'post-types/page-layout.html.twig'];
 Timber::render($templates, $context);
