d41cb84cf06a54ad8ace6cb2bbda34784529d967
Jakub Zak <mail@jakubzak.eu>
2021-11-17 20:08:12 +0100

    Implemented new/updated requirements.

--- theme/single-chemical_element.php
+++ theme/single-chemical_element.php
@@ -4,5 +4,5 @@ use Zakjakub\ChemieZijeTheme\Model\ChemicalElement;
 
 $context = Timber::context();
 $context['element'] = ChemicalElement::getElement(carbon_get_the_post_meta('proton_number'));
-$templates = ['single-customs/single-industry-material.html.twig', 'post-types/page-layout.html.twig'];
+$templates = ['single-customs/single-chemical_element.html.twig', 'post-types/page-layout.html.twig'];
 Timber::render($templates, $context);
