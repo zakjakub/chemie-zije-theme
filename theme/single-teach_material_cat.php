cbf7f065142211dd7cb62bd2f27a087fdb8cd882
Jakub Zak <mail@jakubzak.eu>
2021-11-18 00:17:04 +0100

    Implemented new/updated requirements.

--- theme/single-teach_material_cat.php
+++ theme/single-teach_material_cat.php
@@ -8,8 +8,10 @@ $terms = $context['post']->terms('teach_mat_cat_type');
 $context['subtypes'] = $context['post']->terms('teach_mat_sub_type');
 usort($context['subtypes'], static fn(Term $a, Term $b) => $a->description() <=> $b->description());
 $parts = explode('/', $context['subtypes'][0]?->path() ?? '');
-die(var_dump($parts));
-$firstSubTypeSlug = $parts[array_key_last($parts)] ?? $parts[array_key_last($parts) - 1] ?? 'ostatni';
+$firstSubTypeSlug = $parts[array_key_last($parts)] ?? null;
+if (empty($firstSubTypeSlug)) {
+    $firstSubTypeSlug = $parts[array_key_last($parts) - 1] ?? 'ostatni';
+}
 $context['subtype'] = get_query_var('oblast', $firstSubTypeSlug) ?? $firstSubTypeSlug;
 $context['materials'] = Timber::get_posts(
     new WP_Query([
