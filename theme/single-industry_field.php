b5b92933a7ddafe956441f9344d09434288ebeb9
Jakub Zak <mail@jakubzak.eu>
2021-11-17 21:22:12 +0100

    Implemented new/updated requirements.

--- theme/single-industry_field.php
+++ theme/single-industry_field.php
@@ -1,5 +1,5 @@
 <?php
 
 $context = Timber::context();
-$templates = ['single-customs/single-industry-field.html.twig', 'post-types/page-layout.html.twig'];
+$templates = ['single-customs/single-industry_field.html.twig', 'post-types/page-layout.html.twig'];
 Timber::render($templates, $context);
