9237794e9ea4cf89ae17b549db7e23c1f53e86e2
Jakub Zak <mail@jakubzak.eu>
2021-11-17 20:07:27 +0100

    Implemented new/updated requirements.

--- theme/page_periodical-table.php
+++ theme/page_periodical-table.php
@@ -7,42 +7,7 @@
 
 use Zakjakub\ChemieZijeTheme\Model\ChemicalElement;
 
-const GSHEET_URL = 'https://docs.google.com/spreadsheets/d/1QUGqq7ZxccdSeSu5KVp07AVSUjbFVApMGrCIem0qRGA/edit';
-$data = [];
-$elements = [];
-$elementsData = [];
-if (GSHEET_URL) {
-    try {
-        $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, GSHEET_URL);
-        curl_setopt($curl, CURLOPT_HEADER, 0);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        $sheet = curl_exec($curl);
-        curl_close($curl);
-        preg_match('/(<table[^>]+>)(.+)(<\/table>)/', $sheet, $matches);
-        $data = $matches['0'];
-        $cells_xml = new SimpleXMLElement($data);
-        $cells_json = json_encode($cells_xml, JSON_THROW_ON_ERROR);
-        $cells = json_decode($cells_json, true, 512, JSON_THROW_ON_ERROR);
-        foreach (isset($cells) && is_array($cells) ? $cells['tbody']['tr'] : [] as $rowIndex => $row) {
-            $columnLetter = 'A';
-            foreach ($row['td'] as $columnIndex => $cell) {
-                if (!is_array($cell) || ($cell['div'] ?? false)) {
-                    $elementsData[($rowIndex)][$columnIndex++] = !is_array($cell) ? $cell : $cell['div'];
-                }
-            }
-            $columnLetter++;
-        }
-        $keys = array_flip(array_shift($elementsData));
-        foreach ($elementsData as $elementData) {
-            $elements[] = ChemicalElement::fromRow($elementData, $keys);
-        }
-    } catch (Exception $e) {
-        die('ERROR_ELEMENT: ' . $e->getMessage());
-    }
-}
-
 $context = Timber::context();
-$context['elements'] = $elements;
+$context['elements'] = ChemicalElement::getElements();
 $templates = ["custom-templates/periodical-table.html.twig", 'post-types/page-layout.html.twig'];
 Timber::render($templates, $context);
---
+++ theme/single-chemical_element.php
@@ -0,0 +1,8 @@
+<?php
+
+use Zakjakub\ChemieZijeTheme\Model\ChemicalElement;
+
+$context = Timber::context();
+$context['element'] = ChemicalElement::getElement(carbon_get_the_post_meta('proton_number'));
+$templates = ['single-customs/single-industry-material.html.twig', 'post-types/page-layout.html.twig'];
+Timber::render($templates, $context);
