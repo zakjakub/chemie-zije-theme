9b0ae611f6cfd05d95c5f992e59c9c5d62bda8b0
Jakub Zak <mail@jakubzak.eu>
2021-11-17 22:41:31 +0100

    Implemented new/updated requirements.

---
+++ theme/page_teaching-material-category-videos.php
@@ -0,0 +1,41 @@
+<?php
+/*
+ * Template Name: Výukové materiály - videa
+ * Description: Stránka se seznamem videí sloužících jako výukové materiály.
+ * Template Post Type: teach_material_cat
+ */
+
+use Timber\Term;
+
+$context = Timber::context();
+$context['subtype'] = get_query_var('oblast', 'ostatni');
+$templates = [
+    'custom-templates/teach_material_cat_photos.html.twig',
+    'post-types/teach_material_cat.html.twig',
+    'post-types/page.html.twig',
+];
+$context['categories'] = $context['post']->terms('teach_mat_cat_type');
+$context['subtypes'] = $context['post']->terms('teach_mat_sub_type');
+usort($context['subtypes'], static fn(Term $a, Term $b) => $a->description() <=> $b->description());
+$context['materials'] = Timber::get_posts(
+    new WP_Query([
+        'post_type'      => 'teach_material',
+        'orderby'        => 'title',
+        'order'          => 'ASC',
+        'posts_per_page' => 1000,
+        'tax_query'      => [
+            'relation' => 'AND',
+            [
+                'taxonomy' => 'teach_mat_cat_type',
+                'field'    => 'title',
+                'terms'    => array_map(static fn(Term $term) => $term->__toString(), $context['categories']),
+            ],
+            [
+                'taxonomy' => 'teach_mat_sub_type',
+                'field'    => 'slug',
+                'terms'    => [$context['subtype']],
+            ],
+        ],
+    ])
+);
+Timber::render($templates, $context);
