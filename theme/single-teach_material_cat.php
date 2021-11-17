4b316533f3a82e31762d3d997e1e8cd72ec694bf
Jakub Zak <mail@jakubzak.eu>
2021-11-18 00:13:50 +0100

    Implemented new/updated requirements.

--- theme/single-teach_material_cat.php
+++ theme/single-teach_material_cat.php
@@ -7,8 +7,8 @@ $templates = ['post-types/teach_material_cat.html.twig', 'post-types/page.html.t
 $terms = $context['post']->terms('teach_mat_cat_type');
 $context['subtypes'] = $context['post']->terms('teach_mat_sub_type');
 usort($context['subtypes'], static fn(Term $a, Term $b) => $a->description() <=> $b->description());
-die($context['subtypes'][0]?->path());
 $parts = explode('/', $context['subtypes'][0]?->path() ?? '');
+die($parts);
 $firstSubTypeSlug = $parts[array_key_last($parts)] ?? $parts[array_key_last($parts) - 1] ?? 'ostatni';
 $context['subtype'] = get_query_var('oblast', $firstSubTypeSlug) ?? $firstSubTypeSlug;
 $context['materials'] = Timber::get_posts(
