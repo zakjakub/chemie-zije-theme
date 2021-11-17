d7e542a2265f521302831a166896c1541c47f3f8
Jakub Zak <mail@jakubzak.eu>
2021-11-17 22:12:05 +0100

    Implemented new/updated requirements.

--- src/Post/TeachMaterialPost.php
+++ src/Post/TeachMaterialPost.php
@@ -14,6 +14,21 @@ class TeachMaterialPost extends Post
         );
     }
 
+    final public function safetySymbols(): array
+    {
+        return carbon_get_the_post_meta('safety_symbols') ?? [];
+    }
+
+    final public function difficultySymbols(): array
+    {
+        return carbon_get_the_post_meta('difficulty_symbols') ?? [];
+    }
+
+    final public function tabs(): array
+    {
+        return carbon_get_the_post_meta('tabs') ?? [];
+    }
+
     final public function presentations(): array
     {
         return carbon_get_the_post_meta('presentations') ?? [];
