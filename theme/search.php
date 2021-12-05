564bd3f547c338825e29158dafddff9a978c71da
Jakub Zak <mail@jakubzak.eu>
2021-12-05 23:24:19 +0100

    Implemented new/updated requirements.

--- theme/search.php
+++ theme/search.php
@@ -13,7 +13,7 @@ use Timber\PostQuery;
 
 $templates = ['post-types/search.html.twig', 'post-types/archive.html.twig', 'post-types/page.html.twig'];
 $context = Timber::context();
-$context['title'] = 'Hledání '.get_search_query();
+$context['title'] = 'Vyhledávání "'.get_search_query().'"';
 $context['posts'] = new PostQuery(new WP_Query(get_search_query()));
 
 Timber::render($templates, $context);
