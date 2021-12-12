1ca833459743516630d3f7b76fa2c914ad40a39a
Jakub Zak <mail@jakubzak.eu>
2021-12-12 22:37:36 +0100

    Implemented new/updated requirements.

---
+++ theme/dist/app/main-c7a16a22.js
@@ -0,0 +1,19 @@
+/*!
+ * 
+ * chemieZijeTheme
+ * 
+ * @author Jakub ZAK
+ * @version 0.1.0
+ * @link UNLICENSED
+ * @license UNLICENSED
+ * 
+ * Copyright (c) 2021 Jakub ZAK
+ * 
+ * This software is released under the UNLICENSED License
+ * https://opensource.org/licenses/UNLICENSED
+ * 
+ * Compiled with the help of https://wpack.io
+ * A zero setup Webpack Bundler Script for WordPress
+ */
+(window.wpackiochemieZijeThemeappJsonp=window.wpackiochemieZijeThemeappJsonp||[]).push([[0],{4:function(i,e,n){n(5),i.exports=n(8)},8:function(i,e,n){"use strict";n.r(e);n(7);var o=n(3);n(2),new o.a(".swiper-container",{direction:"horizontal",loop:!1,pagination:{el:".swiper-pagination"},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}})}},[[4,1,2]]]);
+//# sourceMappingURL=main-c7a16a22.js.map
