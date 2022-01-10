c3db0495c2e3697d938a6c3dfc6f854103131c87
Jakub Zak <mail@jakubzak.eu>
2022-01-10 23:01:14 +0100

    Implemented new/updated requirements.

---
+++ theme/single-chem_nomenclature.php
@@ -0,0 +1,27 @@
+<?php
+
+use Timber\Term;
+
+$context = Timber::context();
+$templates = ['single-customs/single-equation-category.html.twig', 'post-types/page.html.twig'];
+
+$context['categories'] = $context['post']->terms('nomenclature_cat');
+
+$context['equations'] = Timber::get_posts(
+    new WP_Query([
+        'post_type'      => 'nomenclat_equation',
+        'orderby'        => 'title',
+        'order'          => 'ASC',
+        'posts_per_page' => 1000,
+        'tax_query'      => [
+            'relation' => 'AND',
+            [
+                'taxonomy' => 'nomenclature_cat',
+                'field'    => 'name',
+                'terms'    => array_map(static fn(Term $term) => $term->__toString(), $context['categories']),
+            ],
+        ],
+    ])
+);
+
+Timber::render($templates, $context);
