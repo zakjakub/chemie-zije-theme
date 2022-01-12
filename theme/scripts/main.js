081dca0d15fa0776b421882ae63b78a3bf14846c
Jakub Zak <mail@jakubzak.eu>
2022-01-12 20:52:40 +0100

    Implemented new/updated requirements.

--- theme/scripts/main.js
+++ theme/scripts/main.js
@@ -115,14 +115,12 @@ function initSelects()
             this.setTextboxValue('');
             this.refreshOptions();
         },
-        render: {
-            option: function (data, escape) {
-                return '<div class="d-flex"><span>' + escape(data.value) + '</span><span class="ms-auto text-muted">' +
-                escape(data.date) + '</span></div>';
-            },
-            item: function (data, escape) {
-                return '<div>' + escape(data.value) + '</div>';
-            },
+    });
+    const tomSelect2 = new TomSelect('#level', {
+        create: true,
+        controlInput: null,
+        onItemAdd: function () {
+            this.refreshOptions();
         },
     });
 }
