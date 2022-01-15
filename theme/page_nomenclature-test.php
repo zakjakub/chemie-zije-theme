d4d80f82fe9d4f4dfcb856507b57e5b11cd2fa29
Jakub Zak <mail@jakubzak.eu>
2022-01-15 22:41:05 +0100

    Implemented new/updated requirements.

--- theme/page_nomenclature-test.php
+++ theme/page_nomenclature-test.php
@@ -4,7 +4,6 @@
  * Template Post Type: page, chem_nomenclature
  */
 
-die('BUG');
 $context = Timber::context();
 $templates = ['custom-templates/nomenclature-test.html.twig', 'post-types/page.html.twig'];
 $context['allCategories'] = \Timber\Timber::get_terms('nomenclature_cat');
