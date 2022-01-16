234dd8a4cd40b149b505b32df8567c2482872ef1
Jakub Zak <mail@jakubzak.eu>
2022-01-16 20:13:27 +0100

    Implemented new/updated requirements.

--- theme/page_nomenclature-test.php
+++ theme/page_nomenclature-test.php
@@ -4,6 +4,9 @@
  * Template Post Type: page, chem_nomenclature
  */
 
+use Timber\Post;
+use Timber\Term;
+
 $context = Timber::context();
 $templates = ['custom-templates/nomenclature-test.html.twig', 'post-types/page.html.twig'];
 $context['allCategories'] = \Timber\Timber::get_terms('nomenclature_cat');
@@ -30,4 +33,30 @@ $context['categories'] = get_query_var('categories', []) ?? [];
 if (is_string($context['categories'])) {
     $context['categories'] = [$context['categories']];
 }
+// Test
+$context['equations'] = Timber::get_posts(
+    new WP_Query([
+        'post_type'      => 'nomenclat_equation',
+        'orderby'        => 'title',
+        'order'          => 'ASC',
+        'posts_per_page' => 1000,
+        'tax_query'      => [
+            'relation' => 'AND',
+            [
+                'taxonomy' => 'nomenclature_cat',
+                'field'    => 'name',
+                'terms'    => array_map(static fn(Term $term) => $term->__toString(), $context['categories']),
+            ],
+        ],
+    ])
+);
+// Filter by level.
+$context['equations'] = array_filter(
+    $context['equations'],
+    static fn(Post $equation) => $equation->meta('_level' <= $context['level']),
+);
+// Randomize order.
+shuffle($context['equations']);
+// Select required amount.
+$context['equations'] = array_slice($context['equations'], 0, $context['count']);
 Timber::render($templates, $context);
