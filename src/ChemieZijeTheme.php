15d61d74271ece078c3ed44822cf475910fbe674
Jakub Zak <mail@jakubzak.eu>
2022-02-14 10:36:30 +0100

    Implemented new/updated requirements.

--- src/ChemieZijeTheme.php
+++ src/ChemieZijeTheme.php
@@ -100,6 +100,47 @@ class ChemieZijeTheme extends Site
         return $vars;
     }
 
+    /**
+     * The [wporg] shortcode.
+     *
+     * Accepts a title and will display a box.
+     *
+     * @param  array  $atts  Shortcode attributes. Default empty.
+     * @param  string  $content  Shortcode content. Default null.
+     * @param  string  $tag  Shortcode tag (name). Default empty.
+     *
+     * @return string Shortcode output.
+     */
+    final public function wporg_shortcode($atts = [], $content = null, $tag = ''): string
+    {
+        // normalize attribute keys, lowercase
+        $atts = array_change_key_case((array)$atts, CASE_LOWER);
+        // override default attributes with user attributes
+        $wporg_atts = shortcode_atts(
+            [
+                'title' => 'WordPress.org',
+            ],
+            $atts,
+            $tag
+        );
+        // start box
+        $o = '<div class="wporg-box">';
+        // title
+        $o .= '<h2>'.esc_html__($wporg_atts['title'], 'wporg').'</h2>';
+        // enclosing tags
+        if (!is_null($content)) {
+            // secure output by executing the_content filter hook on $content
+            $o .= apply_filters('the_content', $content);
+            // run shortcode parser recursively
+            $o .= do_shortcode($content);
+        }
+        // end box
+        $o .= '</div>';
+
+        // return output
+        return $o;
+    }
+
     final public function themeEnqueue(): void
     {
         $this->enqueue->enqueue('app', 'main', []);
@@ -107,6 +148,7 @@ class ChemieZijeTheme extends Site
 
     final public function registerShortcodes(): void
     {
+        add_shortcode('wporg', 'wporg_shortcode');
         add_shortcode('reseni', 'solution');
     }
 
