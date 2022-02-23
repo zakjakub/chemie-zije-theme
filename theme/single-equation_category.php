01c080898c82e663c3e560bba3dfaa055262a943
Jakub Zak <mail@jakubzak.eu>
2022-02-23 14:21:21 +0100

    Implemented new/updated requirements.

--- theme/single-equation_category.php
+++ theme/single-equation_category.php
@@ -1,6 +1,26 @@
 <?php
 
+use Timber\Term;
+
 $context = Timber::context();
 $templates = ['single-customs/single-equation-category.html.twig', 'post-types/page.html.twig'];
-$context['equations'] = carbon_get_the_post_meta('solved_equations');
+## $context['equations'] = carbon_get_the_post_meta('solved_equations');
+$context['categories'] = $context['post']->terms('equation_cat');
+$context['equations'] = Timber::get_posts(
+    new WP_Query([
+        'post_type'      => 'equation',
+        'meta_key'       => '_level',
+        'orderby'        => 'meta_value',
+        'order'          => 'ASC',
+        'posts_per_page' => 1000,
+        'tax_query'      => [
+            'relation' => 'AND',
+            [
+                'taxonomy' => 'equation_cat',
+                'field'    => 'name',
+                'terms'    => array_map(static fn(Term $term) => $term->__toString(), $context['categories']),
+            ],
+        ],
+    ])
+);
 Timber::render($templates, $context);
