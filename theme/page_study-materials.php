a32d1c9241c9013d5c9f19b73d31ae3a4baa668b
Jakub Zak <mail@jakubzak.eu>
2024-01-21 21:34:40 +0100

    Implemented new/updated requirements.

--- theme/page_study-materials.php
+++ theme/page_study-materials.php
@@ -5,6 +5,9 @@
  */
 
 $context = Timber::context();
-$context['posts'] = Timber::get_posts(['post_type' => 'study_material_cat']);
+$context['posts'] = Timber::get_posts([
+    'post_type' => 'study_material_cat',
+    'orderby' => ['priority' => 'ASC', 'menu_order' => 'ASC', 'meta_value' => 'ASC'],
+]);
 $templates = ['custom-templates/study-materials.html.twig', 'post-types/page.html.twig'];
 Timber::render($templates, $context);
